f2c7618cd78aeda97eaf8afb0fa54f05a282084b
GuentherMartin <martin.guenther@arm.com>
2021-08-11 16:39:11 +0200

    Initial release.

---
+++ Projects_ST/Examples_LL/DMA/DMA_CopyFromFlashToMemory_Init/RTE/_DMA_CopyFromFlashToMemory_Init/RTE_Components.h
@@ -0,0 +1,37 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'DMA_CopyFromFlashToMemory_Init' 
+ * Target:  'DMA_CopyFromFlashToMemory_Init' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32g0xx.h"
+
+/* Keil::Device:STM32Cube Framework:STM32CubeMX:1.1.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/* Keil::Device:STM32Cube LL:Common:1.4.0 */
+#define RTE_DEVICE_LL_COMMON
+/* Keil::Device:STM32Cube LL:DMA:1.4.0 */
+#define RTE_DEVICE_LL_DMA
+/* Keil::Device:STM32Cube LL:GPIO:1.4.0 */
+#define RTE_DEVICE_LL_GPIO
+/* Keil::Device:STM32Cube LL:PWR:1.4.0 */
+#define RTE_DEVICE_LL_PWR
+/* Keil::Device:STM32Cube LL:RCC:1.4.0 */
+#define RTE_DEVICE_LL_RCC
+/* Keil::Device:STM32Cube LL:UTILS:1.4.0 */
+#define RTE_DEVICE_LL_UTILS
+/* Keil::Device:Startup:1.4.0 */
+#define RTE_DEVICE_STARTUP_STM32G0XX    /* Device Startup for STM32G0 */
+
+
+#endif /* RTE_COMPONENTS_H */
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # STM32G071B-DISCO_BSP
-STMicroelectronics STM32G071B-DISCO_BSP board support CMSIS Software Pack 
+STMicroelectronics  STM32G071B-DISCO board support CMSIS Software Pack
